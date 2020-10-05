d19b9dd347c8407cf1638f873ce7787a4f0f965c
Kayla Jean Crane <kayla.crane@gmail.com>
2020-10-05 17:29:59 +0200

    set up header links to display correct section only

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,23 @@
-"use strict";
+'use strict';
 
-const header = document.querySelector(".header");
-const headerLinks = header.querySelectorAll("a");
-const headerImage = header.querySelector("img");
+const header = document.querySelector('.header');
+const headerLinks = header.querySelectorAll('span');
+const headerImage = header.querySelector('img');
+const page = document.querySelector('.page');
+const pageSections = document.querySelectorAll('section');
 
-function sizeHeader() {
-  header.classList.toggle("size-header");
+function sizeHeader(event) {
+  page.classList.toggle('size-header');
+  const id = event.currentTarget.dataset.id;
+
+  if (id) {
+    pageSections.forEach(section =>
+      section.classList.remove('visible-section')
+    );
+    const visibleSection = [].find.call( pageSections, section => section.dataset.id === id);
+    visibleSection.classList.add('visible-section');
+  }
 }
 
-headerImage.addEventListener("click", sizeHeader);
-headerLinks.forEach((link) => link.addEventListener("click", sizeHeader));
+headerImage.addEventListener('click', sizeHeader);
+headerLinks.forEach((link) => link.addEventListener('click', sizeHeader));
